9294872ec530a00837563df2fc753b3f9fb828bc
xergio-rh <sergiorueda116@gmail.com>
2025-02-07 23:41:09 -0500

    commit de proyecto

--- Dia 1/Ejercicio1.py
+++
@@ -1,8 +0,0 @@
-## Ejercicio 1: divisores de 3 y 7 hasta 1000
-
-for i in range(1000):  
-    if (i + 1) % 3 == 0 and (i + 1) % 7 == 0:  
-        print(i + 1)  
-
-        
-## Desarrollado por: Sergio Rueda / T.I: 1.097.101.293
